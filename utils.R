81ffb8934ff80a0c0ee8faf9717bc3a241d920f1
Mikhail Dozmorov <mdozmorov@users.noreply.github.com>
2026-01-08 06:59:57 -0500

    Latest

--- utils.R
+++ utils.R
@@ -8,21 +8,21 @@
 #' @param comparison text defining what comparison has been made
 #' @param nplot how many top differential genes to plot
 p.vals <- function(object, p_adj_cutoff = 0.1, subset_by = subset_by, comparison = "DEGs", nplot = 50, print_to_file = TRUE, merge_by = "symbol") {
-  
+
   genes.ABC <- topTags(object, n = Inf, p.value = p_adj_cutoff) %>% as.data.frame # Get all significant genes
   # Proceed only if some results returned
   if (nrow(genes.ABC) > 0) {
     if (merge_by == "symbol") {
-      genes.ABC <- left_join(data.frame(genes.ABC, stringsAsFactors = FALSE), 
+      genes.ABC <- left_join(data.frame(genes.ABC, stringsAsFactors = FALSE),
                              gene_annotations, by = c("genes" = "symbol")) # Attach annotations
     }
     if (merge_by == "ensgene") {
-      genes.ABC <- left_join(data.frame(genes.ABC, stringsAsFactors = FALSE), 
+      genes.ABC <- left_join(data.frame(genes.ABC, stringsAsFactors = FALSE),
                              gene_annotations, by = c("genes" = "ensgene")) # Attach annotations
     }
     # Sort by LR
     genes.ABC <- genes.ABC[order(genes.ABC$LR, decreasing = TRUE), ]
-    
+
     if (print_to_file) {
       # Save most significant genes
       save_res(genes.ABC, fileName_rna, wb = wb, sheetName = comparison) # Save gene list
@@ -37,33 +37,33 @@ p.vals <- function(object, p_adj_cutoff = 0.1, subset_by = subset_by, comparison
 genes_to_heatmap <- function(object, edgeR.dgelist = edgeR.dgelist, nplot = 50, subset_by = c("A", "B"), comparison = "C231_noCRISPR_vs_Negative", clustmethod = "ward", width = 6, height = 8, print_to_file = TRUE) {
   # Select columns to subset
   if (!is.na(subset_by)){
-    subset_index <- grepl(paste(subset_by, collapse = "|"), sample_annotation_subset$Group) 
+    subset_index <- grepl(paste(subset_by, collapse = "|"), sample_annotation_subset$Group)
   } else {
     subset_index <- 1:ncol(cpm(edgeR.dgelist, log = TRUE))
   }
   # # Order object by the most on-average significant genes
-  # object <- object[ order( (rank(object$FDR.x) + rank(object$FDR.y)) / 2), ] 
+  # object <- object[ order( (rank(object$FDR.x) + rank(object$FDR.y)) / 2), ]
   # # Save most significant genes
   # fileName <- paste0("results/DEGs_", comparison,  "_hg19.xlsx") # Construct filename
   # unlink(fileName) # Delete file, if exists
   # write.xlsx2(object, fileName, sheetName = comparison, row.names = FALSE) # Save gene list
-  
+
   # Plot top genes
-  genes.to.plot <- object[1:nplot, c("genes", "symbol")] 
+  genes.to.plot <- object[1:nplot, c("genes", "symbol")]
   genes.to.plot <- genes.to.plot[match(edgeR.dgelist@.Data[[3]]$genes[edgeR.dgelist@.Data[[3]]$genes %in% genes.to.plot$genes], genes.to.plot$genes), ] # Make the same order as the genes subsetted from the whole dataset
   matrix.to.plot <- cpm(edgeR.dgelist, log = TRUE, normalized.lib.sizes=TRUE)[edgeR.dgelist@.Data[[3]]$genes %in% genes.to.plot$genes, subset_index] # * edgeR.dgelist@.Data[[2]]$norm.factors[subset_index] # Subset matrix to plot
-  
+
   annotation_col <- data.frame(Group = sample_annotation_subset$Group[subset_index])
   rownames(annotation_col) <- colnames(cpm(edgeR.dgelist)[, subset_index])
-  
+
   # If too many genes, do not plot gene names
   if (nplot > 50) {
     genes.to.plot$symbol <- ""
   }
-  
+
   # Save to PDF
   if (print_to_file) {
-    dev.off()
+    # dev.off()
     pdf(paste0("results/Figure_Heatmap_", comparison, "_", nplot, ".pdf"), width = width, height = height)
     pheatmap(matrix.to.plot, color = col3, clustering_method = "ward", treeheight_row = FALSE, treeheight_col = FALSE, annotation_col = annotation_col, labels_row = genes.to.plot$symbol, scale = "row")
     dev.off()
@@ -77,7 +77,7 @@ genes_to_heatmap <- function(object, edgeR.dgelist = edgeR.dgelist, nplot = 50, 
 # A function to plot a barplot of selected genes in selected conditions
 genes_to_boxplot <- function(selected_genes = c("NF1", "SND1", "MTDH"), subset_by = c("C", "AA")) {
   # Select columns to subset
-  # subset_index <- grepl(paste(subset_by, collapse = "|"), sample_annotation$Group) 
+  # subset_index <- grepl(paste(subset_by, collapse = "|"), sample_annotation$Group)
   subset_index <- sample_annotation_subset$Group == subset_by[1] | sample_annotation_subset$Group == subset_by[2]
   # Select EntrezIDs to plot
   genes.to.plot <- gene_annotations[gene_annotations$symbol %in% selected_genes, ]
@@ -85,7 +85,7 @@ genes_to_boxplot <- function(selected_genes = c("NF1", "SND1", "MTDH"), subset_b
   # Expression of genes to plot
   matrix.to.plot <- cpm(edgeR.dgelist, log = TRUE)
   index_to_keep  <- edgeR.dgelist@.Data[[3]]$genes %in% genes.to.plot$ensgene
-  matrix.to.plot <- matrix.to.plot[index_to_keep, subset_index, drop = FALSE] 
+  matrix.to.plot <- matrix.to.plot[index_to_keep, subset_index, drop = FALSE]
   rownames(matrix.to.plot) <- edgeR.dgelist@.Data[[3]]$genes[index_to_keep]
   colnames(matrix.to.plot) <- sample_annotation_subset$Group[subset_index]
   # Add genes
@@ -110,13 +110,13 @@ genes_to_boxplot <- function(selected_genes = c("NF1", "SND1", "MTDH"), subset_b
 summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                       conf.interval=.95, .drop=TRUE) {
   library(plyr)
-  
+
   # New version of length which can handle NA's: if na.rm==T, don't count them
   length2 <- function (x, na.rm=FALSE) {
     if (na.rm) sum(!is.na(x))
     else       length(x)
   }
-  
+
   # This does the summary. For each group's data frame, return a vector with
   # N, mean, and sd
   datac <- ddply(data, groupvars, .drop=.drop,
@@ -128,18 +128,18 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                  },
                  measurevar
   )
-  
-  # Rename the "mean" column    
+
+  # Rename the "mean" column
   datac <- rename(datac, c("mean" = measurevar))
-  
+
   datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  
+
   # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval: 
+  # Calculate t-statistic for confidence interval:
   # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
   ciMult <- qt(conf.interval/2 + .5, datac$N-1)
   datac$ci <- datac$se * ciMult
-  
+
   return(datac)
 }
 
